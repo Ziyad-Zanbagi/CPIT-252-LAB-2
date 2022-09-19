c75b9106ccef9f687aa6f239f6a298b36f68b539
Ziyad-Zanbagi <104837671+Ziyad-Zanbagi@users.noreply.github.com>
2022-09-19 20:52:52 +0300

    Add files via upload

---
+++ src/main/java/com/example/Cpit252Lab2.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.example;
+
+/**
+ *
+ * @author 1937512
+ */
+public class Cpit252Lab2 {
+
+  /**
+   * @param args the command line arguments
+   */
+  public static void main(String[] args) {
+    Order o1 = new Order();
+    Shipment sh1 = new Shipment("Ahmed",
+        "248 NE. Pleasant St. Niceville, FL 32578",
+        "123-477-0001");
+  }
+
+}
